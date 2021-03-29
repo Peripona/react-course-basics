a15853391ef08c64910ac4e32d64791d75a1c7a2
Tarandeep Singh <peripona@yahoo.com>
2021-03-29 16:42:25 +0200

    tranition cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Card.css';
 
 const Card = (props:any) => (
-    <div>
-        <h1>{props.user.name}</h1>
+    <div className="card">
+        <h1 className="elipsis">{props.user.name}</h1>
         <img src={`https://robohash.org/${props.user.id}?set=set1&size=180x180`} alt={props.user.name} />
     </div>
 );
--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Card from '../Card/Card';
+import './CardList.css';
 
-const CardList = (props: any) => (
-    props.users.map((user:any) => <Card key={user.id} user={user} />)
+const CardList = (props: any) => (<div className="card-list">
+    {props.users.map((user:any) => <Card key={user.id} user={user} />)}
+    </div>
 )
 
 export default CardList;
